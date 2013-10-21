3fa96672e7f4f0f89df0b53642bc1c3c547abb53
hemalchevli <hemal_chevli@yahoo.com>
2013-10-21 13:57:01 +0530

    RT on in english

--- twitterboy_server.py
+++ twitterboy_server.py
@@ -2,7 +2,7 @@
 My twitter retweet bot, stor the search results in array and retweet them
 '''
 from random import randint
-import time 
+import time
 import sys
 import twitter
 
@@ -16,26 +16,26 @@ find = ['arduino','rasberry pi','linux','ubuntu','android','automation','python'
 
 #loop through all
 for f in find:
-		results = api.GetSearch(f)
-		print "\tfound %s for %s" %(len(results),f)
-		for statusObj in results:
-			try:
-				#retweet
-				#ignore if contains a mention
-				#print 'Posting in reply to @%s: %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
-				s = statusObj.text.encode('ascii', 'replace')
-				#s.find('$')==-1 # not found
-				#s.find('$')!=-1 # found
-				if s.find('@') == -1:
-					print "NO mention retweeting..."
-					print statusObj.id
-					api.PostRetweet(statusObj.id)
-					delay = randint(120,180)
-					print delay
-					time.sleep(delay) #prevent account suspension 
-					continue
-				else:
-					print "has a mention" # if a tweet has a mention it will skip it and move to next
-				
-			except Exception:
-				print "Unexpected error:", sys.exc_info()[0:2]
+                results = api.GetSearch(f)
+                print "\tfound %s for %s" %(len(results),f)
+                for statusObj in results:
+                        try:
+                                #retweet
+                                #ignore if contains a mention
+                                #print 'Posting in reply to @%s: %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
+                                s = statusObj.text.encode('ascii', 'replace')
+                                #s.find('$')==-1 # not found
+                                #s.find('$')!=-1 # found
+                                if s.find('@') == -1:
+                                        print "NO mention retweeting..."
+                                        print statusObj.id
+                                        api.PostRetweet(statusObj.id)
+                                        delay = randint(120,180)
+                                        print delay
+                                        time.sleep(delay) #prevent account suspension
+                                        continue
+                                else:
+                                        print "has a mention" # if a tweet has a mention it will skip it and move to next
+                                
+                        except Exception:
+                                print "Unexpected error:", sys.exc_info()[0:2]
